313dc0227237a93b2abd31cd398a067f852acfcc
majorro <majorrowk@gmail.com>
2026-03-03 22:52:32 +0300

    remove serilog

--- src/DoujinMusicReposter.App/Setup/HostApplicationBuilderExtensions.cs
+++ src/DoujinMusicReposter.App/Setup/HostApplicationBuilderExtensions.cs
@@ -1,13 +1,9 @@
-﻿using System.Threading.Channels;
+﻿using System.Text.Json;
+using System.Threading.Channels;
 using DoujinMusicReposter.App.Services;
 using DoujinMusicReposter.App.Services.Interfaces;
-using DoujinMusicReposter.App.Setup.Configuration;
 using DoujinMusicReposter.App.Workers;
 using DoujinMusicReposter.Vk.Dtos;
-using Majorro.Common.Setup.Extensions;
-using Microsoft.Extensions.Options;
-using Serilog;
-using Serilog.Events;
 
 namespace DoujinMusicReposter.App.Setup;
 
@@ -15,28 +11,12 @@ public static class HostApplicationBuilderExtensions
 {
     public static IHostApplicationBuilder AddLogging(this IHostApplicationBuilder builder)
     {
-        builder.Configure<SerilogConfig>();
-
-        // TODO: move to config
-        // TODO: implement a custom enricher for source context: https://stackoverflow.com/a/75332511/10850306
-        const string outputTemplate = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] ({SourceContext}) {Message:lj}{NewLine}{Exception}";
-        builder.Services.AddSerilog((sp, config) =>
+        builder.Logging.ClearProviders();
+        builder.Logging.AddJsonConsole(x =>
         {
-            var serilogConfig = sp.GetRequiredService<IOptions<SerilogConfig>>().Value;
-
-            config
-                .MinimumLevel.Information()
-                .MinimumLevel.Override("System.Net.Http.HttpClient", LogEventLevel.Warning)
-                .Enrich.FromLogContext()
-                .WriteTo.Console(outputTemplate: outputTemplate)
-                .WriteTo.Telegram(
-                    serilogConfig.TelegramBotToken,
-                    serilogConfig.TelegramChatId,
-                    restrictedToMinimumLevel: LogEventLevel.Error)
-                .WriteTo.File(
-                    "logs/log.txt",
-                    rollingInterval: RollingInterval.Day,
-                    outputTemplate: outputTemplate);
+            x.IncludeScopes = true;
+            if (!builder.Environment.IsProduction())
+                x.JsonWriterOptions = new JsonWriterOptions() { Indented = true };
         });
 
         return builder;
